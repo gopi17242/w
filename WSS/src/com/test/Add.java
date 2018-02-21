dc99d7076bdec7c71e9171375d53e1ee795ecd98
gopi17242 <31234068+gopi17242@users.noreply.github.com>
2018-02-21 14:29:16 +0530

    test

---
+++ WSS/src/com/test/Add.java
@@ -0,0 +1,18 @@
+package com.test;
+
+public class Add {
+	
+	public int add(int a, int b) {
+		
+		int c = a + b;
+		return c;
+	}
+
+	
+public int sub(int a, int b) {
+		
+	int c = a - b;
+		return c;
+	}
+
+}
